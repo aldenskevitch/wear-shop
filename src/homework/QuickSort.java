c2e889950022a2d44e668d1d2506f4ea0d962838
AlexanderDenskevitch <84723495+AlexanderDenskevitch@users.noreply.github.com>
2021-09-29 01:27:06 +0300

    style edit

--- src/homework/QuickSort.java
+++ src/homework/QuickSort.java
@@ -6,10 +6,13 @@ import java.util.Random;
 public class QuickSort {
     public static void main(String[] args) {
         Random rand = new Random();
+
         int[] array = new int[10];
-        for (int i = 0; i < array.length; i++)
+        for (int i = 0; i < array.length; i++) {
             array[i] = rand.nextInt(100);
+        }
         System.out.println(Arrays.toString(array));
+
         quickSort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
     }
