f7eda6f4829e52c95b0396c80b86bd052c01d5f3
AlexanderDenskevitch <84723495+AlexanderDenskevitch@users.noreply.github.com>
2021-11-04 03:36:49 +0300

    ExecutorsClass has ben added

--- src/main/java/com/solvd/wearshopproject/multithreading/Main.java
+++ src/main/java/com/solvd/wearshopproject/multithreading/Main.java
@@ -1,35 +1,46 @@
 package com.solvd.wearshopproject.multithreading;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
 
         ConnectionPool connectionPool = ConnectionPool.getInstance(5);
 
-        IntStream.range(0, 10)
+        IntStream.range(0, 100)
                 .boxed()
                 .forEach(index -> {
                     Thread thread = new ThreadClass(connectionPool);
                     thread.start();
-                    try {
-                        Thread.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    sleep(500);
                 });
 
-        IntStream.range(0, 10)
+        IntStream.range(0, 100)
                 .boxed()
                 .forEach(index -> {
                     RunnableClass runnable = new RunnableClass(connectionPool);
                     Thread thread = new Thread(runnable);
                     thread.start();
-                    try {
-                        Thread.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    sleep(500);
                 });
+
+        ExecutorsClass.start();
+
+    }
+
+    public static void sleep(Integer mils) {
+        try {
+            Thread.sleep(mils);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
